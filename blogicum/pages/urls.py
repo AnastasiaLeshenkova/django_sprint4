3fbe0a93a2726e658eabc08fb97ecd1dc5f5c297
Anastasya <nastenaleshenkova@mail.ru>
2025-03-17 19:37:42 +0700

    Add first commit

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,11 @@
+"""Jopa"""
+from django.urls import path
+
+from pages import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('rules/', views.RulesView.as_view(), name='rules'),
+]
